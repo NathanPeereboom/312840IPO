eef2c3e74b76de5a7360e55aacfe02290228b079
NathanPeereboom <47399969+NathanPeereboom@users.noreply.github.com>
2019-02-11 09:39:16 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿/*Nathan Peereboom
+ * Feb 8, 2019
+ * This is my second program, which uses IPO logic
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312840IPO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double length;
+            double width;
+            double area;
+
+            double.TryParse(txtLength.Text, out length);
+            double.TryParse(txtWidth.Text, out width);
+
+            area = length * width;
+
+            lblOutput.Content = "The area is: " + area.ToString("0.00");
+
+        }
+    }
+}
